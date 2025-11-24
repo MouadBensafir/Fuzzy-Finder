514b18ee0780dcf5cb1d960a7b96df94d8de5abc
Mouad Bensafir <mouadensafir@gmail.com>
2025-11-24 18:11:03 +0100

    first commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,13 @@
+module org.example.fuzzy_app {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens org.example.fuzzy_app to javafx.fxml;
+    exports org.example.fuzzy_app;
+}
