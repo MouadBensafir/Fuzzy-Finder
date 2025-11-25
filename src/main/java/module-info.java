02bd1e14fb2b118096d59d398993d51e182889ef
Akito-sama <ahmed.quadimi@um6p.ma>
2025-11-25 13:00:53 +0100

    adding Normalization

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,10 +2,12 @@ module org.example.fuzzy_app {
     requires javafx.controls;
     requires javafx.fxml;
     requires javafx.web;
+    requires javafx.graphics;   // <-- OBLIGATOIRE
+    requires javafx.media;      // <-- pour FXGL
+    requires javafx.swing;      // <-- si FXGL ou TilesFX utilisent Swing
 
     requires org.controlsfx.controls;
     requires org.kordamp.bootstrapfx.core;
-    requires eu.hansolo.tilesfx;
     requires com.almasb.fxgl.all;
 
     opens org.example.fuzzy_app to javafx.fxml;
